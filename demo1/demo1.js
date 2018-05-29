a0eeb250fbd720053c6ea950f20608b52d8c645d
Dave Jones <dmj111@gmail.com>
2018-05-28 22:12:33 -0400

    add fourth method, cleanup output.

--- demo1/demo1.js
+++ demo1/demo1.js
@@ -1,4 +1,4 @@
-/* global  window, alert, console */
+/* global  window, alert, console, document */
 
 (function() {
     'use strict';
@@ -59,7 +59,7 @@
         const data = new Int8Array((n + 1) / 2);
         data[0] = 1;
         for(let i = 1; i < data.length; i+= 1) {
-            if (data[i] == 0) {
+            if (data[i] === 0) {
                 let k = 2 * i + 1;
                 let u = i * (1 + k);
                 if (u > data.length) { break; }
@@ -76,31 +76,59 @@
         }
         return result;
     }
+
+    // Naive sieve, use wheel for skipping.
+    // use Int8Array for more compact data.
+    function prime4(n) {
+        const data = new Int8Array((n + 1) / 2);
+        data[0] = 1;
+
+        // clear 3's
+        for (let j = 4; j < data.length; j += 3) {
+            data[j] = 1;
+        }
+
+        let step = 2;
+        let u = 1;
+        const result = [2, 3];
+        for(let i = 2; i < data.length; i += step) {
+            if (data[i] === 0) {
+                let k = 2 * i + 1;
+                result.push(k);
+                if (u < data.length) {
+                    u = i * (1 + k); // (i + 2 * i**2 + i
+                    for (let j = u; j < data.length; j += k) {
+                        data[j] = 1;
+                    }
+                }
+            }
+            step = 3 - step;
+        }
+
+        return result;
+    }
+
     function run() {
         let result = document.getElementById('result');
+        const N = 2000000;
         while (result.hasChildNodes()) {
             result.removeChild(result.lastChild);
         }
-        console.log(prime3(20));
+
         let table = [];
-        for(let i = 0; i < 10; i += 1) {
-            console.time('prime3');
-            let now = new Date().getTime();
-            console.log(prime3(1000000).length);
-            table.push({fcn: 'prime3', time: new Date().getTime() - now});
-            console.timeEnd('prime3');
 
-            console.time('prime2');
-            now = new Date().getTime();
-            console.log(prime2(1000000).length);
-            table.push({fcn: 'prime2', time: new Date().getTime() - now});
-            console.timeEnd('prime2');
-
-            console.time('prime1');
-            now = new Date().getTime();
-            console.log(prime1(1000000).length);
-            table.push({fcn: 'prime1', time: new Date().getTime() - now});
-            console.timeEnd('prime1');
+        for (let item of [['prime1', prime1],
+                          ['prime2', prime2],
+                          ['prime3', prime3],
+                          ['prime4', prime4],
+                         ]) {
+            for(let i = 0; i < 10; i += 1) {
+                console.time(item[0]);
+                let now = new Date().getTime();
+                console.log(item[1](N).length);
+                table.push({fcn: item[0], time: new Date().getTime() - now});
+                console.timeEnd(item[0]);
+            }
         }
         console.table(table);
     }
--- demo1/index.md
+++ demo1/index.md
@@ -1,6 +1,12 @@
-# This is a test
+# Test of javascript / jekyll / etc
+
+
+Generate prime numbers.  View the results in the javascript console.
+
 
 <button type='button' id='run'>run example </button>
+
+[view source](demo1.js)
 
 <div id='result'>
 </div>
