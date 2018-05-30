84eacaae0e553492c88500067a09727de6ad3b36
Dave Jones <dmj111@gmail.com>
2018-05-29 20:09:00 -0400

    Add notes to setup.

--- _setup.sh
+++ _setup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Setup the environment
+# rvm gemset create jekyll
+# rvm gemset use jekyll
+# gem install bundler
+
+
 source ~/.rvm/scripts/rvm
 rvm gemset use jekyll
 bundle exec jekyll serve
+
+
+
+# useful commands
+# rvm list known
+# rvm gemdir
+# rvm gemset delete jekyll
+# rvm gemset empty jekyll
